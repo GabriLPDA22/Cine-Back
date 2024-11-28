caf4f4af70052df79aa4b5e6d80cb2ca85cab5ab
Gabriel Saiz <gsaiz.bajo@gmail.com>
2024-11-28 23:08:44 +0100

    añadido id en sesion

--- cine_web_app/back_end/Models/Sesion.cs
+++ cine_web_app/back_end/Models/Sesion.cs
@@ -4,6 +4,9 @@ namespace cine_web_app.back_end.Models
 {
 public class Sesion
     {
+
+
+        public int Id { get; set; }  // Identificador de la sesión
         public DateTime Fecha { get; set; }  // Fecha de la sesión
         public string Hora { get; set; }
         public string Sala { get; set; }
