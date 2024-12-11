30e418cc30ec99d2ecd37e7eb606ef6de5c806df
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2024-12-11 19:59:24 +0100

    Modelo creado

---
+++ cine_web_app/back_end/Models/Comentario.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cine_web_app.back_end.Models
+{
+public class Comentario
+    {
+        public string Id { get; set; }
+        public string Usuario { get; set; }
+        public string Texto { get; set; }
+    }
+}
